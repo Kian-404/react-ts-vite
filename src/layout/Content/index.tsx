cb76f0cd9a7fa4b15ac5cf3532ded1fcf466c169
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-09-10 17:31:41 +0800

    Add: 添加content路由组件

--- src/layout/Content/index.tsx
+++ src/layout/Content/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Layout } from 'antd';
 import './index.css';
+import Router from '../../routers'
 const { Content } = Layout;
 const MContent = () => {
   return (
     <Content className="site-layout-background">
-      Content
+      <Router/>
     </Content>
   )
 }
