9bb55b67fffabb862c12bfdc1a291e50702669a5
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-09-10 17:29:21 +0800

    Add: 添加路由组件

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './redux'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
-  ,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 )
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../views/Home'
+import About from '../views/About'
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/about" component={About}></Route>
+    </Switch>
+  )
+}
+
+export default Router
