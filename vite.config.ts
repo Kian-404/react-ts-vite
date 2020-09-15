b7f46470453b8aabb4e0a385aed9c6ec92825f8d
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-09-15 11:42:18 +0800

    Style:配置vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import * as reactPlugin from 'vite-plugin-react'
 import type { UserConfig } from 'vite'
+import path from 'path'
 
 const config: UserConfig = {
   jsx: 'react',
-  plugins: [reactPlugin]
+  plugins: [reactPlugin],
+  // 导入文件夹别名
+  alias: {
+    '/@/': path.resolve(__dirname, './src'),
+    '/@views/': path.resolve(__dirname, './src/views'),
+    '/@components/': path.resolve(__dirname, './src/components'),
+    '/@utils/': path.resolve(__dirname, './src/utils'),
+  },
+  optimizeDeps: {
+    include: ['echarts',]
+  }
 }
 
 export default config
