798c9a3a069bfa6e341d5a832fbc485d262e617e
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-09-17 11:17:41 +0800

    :construction: 完善Header组件

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,25 +1,56 @@
 import React, { ElementType } from 'react';
 import { connect } from 'react-redux'
 import { toggleCollapsed } from '../../redux/actions/index'
-import { Layout } from 'antd';
+import { Layout, Badge, Menu, Dropdown, Avatar } from 'antd';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
+  BellOutlined,
+  DownOutlined,
+  LogoutOutlined,
+  UserOutlined
 } from '@ant-design/icons';
 import './index.css'
-
 const { Header, } = Layout;
 interface props {
   collapsed: Boolean,
   setCollapesd: Function
 }
-const MyHeader:ElementType = ({ collapsed, setCollapesd }: props) => {
+
+const menu = (
+  <Menu>
+    <Menu.Item >
+      <UserOutlined />
+      详情
+    </Menu.Item>
+    <Menu.Item >
+      <LogoutOutlined />
+        退出
+    </Menu.Item>
+  </Menu>
+);
+const MyHeader: ElementType = ({ collapsed, setCollapesd }: props) => {
   return (
-    <Header className="site-layout-background" style={{ padding: 0 }}>
-      {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-        className: 'trigger',
-        onClick: () => setCollapesd(!collapsed),
-      })}
+    <Header className="site-layout-background header" style={{ padding: 0 }}>
+      <div className="toggle-menu">
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+          className: 'trigger',
+          onClick: () => setCollapesd(!collapsed),
+        })}
+      </div>
+      <div className="header-content">
+
+        <div className="option-item">
+          <Dropdown overlay={menu} placement="bottomCenter" arrow>
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+              <Avatar style={{ backgroundColor: '#ffbf00' }}>Kian</Avatar> <DownOutlined />
+            </a>
+          </Dropdown>
+        </div>
+        <div className="option-item">
+          <Badge count={5}><BellOutlined /></Badge>
+        </div>
+      </div>
     </Header>
   )
 }
