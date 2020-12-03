88ed48f9387affecf9cd5c44fe2f9732da73b924
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-12-03 11:12:10 +0800

    :card_file_box: 添加mock数据接口

--- src/api/mock.tsx
+++ src/api/mock.tsx
@@ -1,8 +1,29 @@
 import request from '../utils/request';
 import '../mocks/index'
-export const getMockData = (params: any) =>
-  request({
+
+
+/**
+ * @description: 获取Mock数据
+ * @param {*}
+ * @return {*}
+ */
+export const getMockData = (params: any) => {
+  return request({
     url: '/mock',
     method: 'get',
     params
   })
+}
+
+/**
+ * @description: 获取文章列表
+ * @param {*}
+ * @return {*}
+ */
+export const getArtileList = (params: any) => {
+  return request({
+    url: '/artile-list',
+    method: 'get',
+    params
+  })
+}
