ff13de446ccf4c9b7d63e974259992807b3737e1
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-12-07 13:48:59 +0800

    :dizzy: 添加404页面使用lottiefiles

--- src/views/ErrorPage/404.tsx
+++ src/views/ErrorPage/404.tsx
@@ -1,22 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Result, Button } from 'antd'
-
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import * as animationData from '../../assets/error-page/404-page.json'
 const ERR_404 = () => {
   const routerHome = () => {
 
   }
+
+  const clickHere = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+  }
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        <Button type="primary">
-          <Link to="/">Back Home</Link>
-        </Button>
-      }
-    />
+    <div className="err-404">
+      <Player
+        autoplay
+        loop
+        src={animationData}
+        style={{ "height": '400px', "max-width": '800px' }}
+      >
+        <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+      </Player>
+    </div>
+
+    // <Result
+    //   status="404"
+    //   title="404"
+    //   subTitle="Sorry, the page you visited does not exist."
+    //   extra={
+    //     <Button type="primary">
+    //       <Link to="/">Back Home</Link>
+    //     </Button>
+    //   }
+    // />
   )
 }
 
