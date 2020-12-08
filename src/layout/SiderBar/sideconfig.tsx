5d6df1df139c9fce036a28ba2ee32bb32d4f77c7
tian_jianen <tian_jianen@venusgroup.com.cn>
2020-12-08 14:41:06 +0800

    :art: sidebar添加key字段

--- src/layout/SiderBar/sideconfig.tsx
+++ src/layout/SiderBar/sideconfig.tsx
@@ -23,6 +23,7 @@ import {
   TableOutlined
 } from '@ant-design/icons'
 export interface ItemProps {
+  key:string,
   title: String,
   icon: any,
   children?: Array<ItemProps>,
@@ -31,21 +32,25 @@ export interface ItemProps {
 }
 export const SiderItems: Array<ItemProps> = [
   {
+    key:'1',
     title: '首页',
     url: '/',
     icon: <DashboardOutlined />
   },
   {
+    key:'2',
     title: '文章管理',
     url: '/main/article-list',
     icon: <EditOutlined />,
     children: [
       {
+        key:'2-1',
         title: '文章列表',
         url: '/main/article-list',
         icon: <OrderedListOutlined />,
       },
       {
+        key:'2-2',
         title: '新建文章',
         url: '/main/article-new',
         icon: <EditOutlined />,
@@ -53,26 +58,31 @@ export const SiderItems: Array<ItemProps> = [
     ]
   },
   {
+    key:'3',
     title: '页面管理',
     url: '/main/page-list',
     icon: <ProfileOutlined />
   },
   {
+    key:'4',
     title: '扩展组件',
     url: '/main/',
     icon: <AppstoreOutlined />,
     children: [
       {
+        key:'4-1',
         title: '富文本编辑器',
         url: '/main/rishtext-editor',
         icon: <FormOutlined />,
       },
       {
+        key:'4-2',
         title: '代码编译器',
         url: '/main/code-editor',
         icon: <CodeOutlined />,
       },
       {
+        key:'4-3',
         title: 'Markdown编译器',
         url: '/main/markdown-editor',
         icon: <FileMarkdownOutlined />,
@@ -80,26 +90,31 @@ export const SiderItems: Array<ItemProps> = [
     ]
   },
   {
+    key:'5',
     title: 'Table组件',
     url: '/table/',
     icon: <InsertRowBelowOutlined />,
     children: [
       {
+        key:'5-1',
         title: '基本表格',
         url: '/main/table-base',
         icon: <TableOutlined />,
       },
       {
+        key:'5-2',
         title: '动态表格',
         url: '/main/table-dynamic',
         icon: <TableOutlined />,
       },
       {
+        key:'5-3',
         title: '编辑表格',
         url: '/main/table-edit',
         icon: <TableOutlined />,
       },
       {
+        key:'5-4',
         title: '多数据表格',
         url: '/main/table-bigdata',
         icon: <TableOutlined />,
@@ -107,46 +122,55 @@ export const SiderItems: Array<ItemProps> = [
     ]
   },
   {
+    key:'6',
     title: '评论管理',
     url: '/main/main/comment-list',
     icon: <MessageOutlined />
   },
   {
+    key:'7',
     title: '文件管理',
     url: '/main/file-list',
     icon: <FileMarkdownOutlined />
   },
   {
+    key:'8',
     title: '搜索记录',
     url: '/main/search-list',
     icon: <FileSearchOutlined />
   },
   {
+    key:'9',
     title: '访问统计',
     url: '/history-list',
     icon: <LineChartOutlined />
   },
   {
+    key:'10',
     title: '邮件管理',
     url: '/email-list',
     icon: <MailOutlined />
   },
   {
+    key:'11',
     title: '权限页面',
     url: '/main/auth-page',
     icon: <MehOutlined />,
     children: [
       {
+        key:'11-1',
         title: '403',
         url: '/main/403',
         icon: <RobotOutlined />
       },
       {
+        key:'11-2',
         title: '404',
         url: '/main/404',
         icon: <RobotOutlined />
       },
       {
+        key:'11-3',
         title: '500',
         url: '/main/500',
         icon: <RobotOutlined />
@@ -155,21 +179,25 @@ export const SiderItems: Array<ItemProps> = [
 
   },
   {
+    key:'12',
     title: '系统设置',
     url: '/main/system-setting',
     icon: <SettingOutlined />,
     children: [
       {
+        key:'12-1',
         title: '个人中心',
         url: '/main/about',
         icon: <UserOutlined />
       },
       {
+        key:'12-2',
         title: '安全设置',
         url: '/main/safesetting',
         icon: <SafetyOutlined />
       },
       {
+        key:'12-3',
         title: '权限设置',
         url: '/main/securitysetting',
         icon: <SecurityScanOutlined />
